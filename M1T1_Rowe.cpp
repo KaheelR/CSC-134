67cd73080de86cbce70e6a192c1aae7db02896d8
KaheelR <rowek4671@student.faytechcc.edu>
2026-02-02 23:10:29 -0500

    Add files via upload

---
+++ M1T1_Rowe.cpp
@@ -0,0 +1,19 @@
+// CSC 134
+// M1T1
+// Kaheel Rowe
+// 02/02/2026
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Greet the user
+    cout << "Hello, user!" << endl;
+    cout << "What's your name?" << endl;
+    string name;
+    cin >> name;
+    cout << "Nice to meet you, " << name << endl;
+
+
+    return 0;
+}
