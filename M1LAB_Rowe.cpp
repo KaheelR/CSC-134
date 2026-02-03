cbb9b61f79b86f090ea47f41199d4ae9819927d0
KaheelR <rowek4671@student.faytechcc.edu>
2026-02-02 23:06:01 -0500

    Add files via upload

---
+++ M1LAB_Rowe.cpp
@@ -0,0 +1,27 @@
+// CSC 134
+// M1LAB
+// Your Name
+// The Date
+#include <iostream>
+using namespace std;
+int main() {
+// This program will simulate an apple orchard.
+// The owner’s name
+string name = “Your Name”
+// number of apples owned
+int apples = 100
+// price per apple
+double pricePerApple = 0.25
+// calculate the total price of the apples
+// TODO
+// print all the information about the orchard
+// print all the information about the orchard
+cout << "Welcome to " << name;
+cout << " ’s apple orchard." << endl;
+cout << "We have " << apples;
+cout << " apples in stock" << endl;
+cout << "Apples are currently $";
+cout << pricePerApple << " each." << endl;
+cout << “Welcome to “ << name;
+cout << “ ’s apple orchard.” << endl;
+}
