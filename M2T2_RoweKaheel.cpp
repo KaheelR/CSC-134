de8e64b2c9b71e39b48187bdb38d4174cfb44404
KaheelR <rowek4671@student.faytechcc.edu>
2026-02-16 22:12:55 -0500

    Add files via upload

---
+++ M2T2_RoweKaheel.cpp
@@ -0,0 +1,46 @@
+/*
+CSC 134
+M2T2 - Receipt Calculator
+Kaheel Rowe
+02/16/2026
+*/
+
+#include <iostream>
+#include <iomanip> // for the 2 decimal places
+using namespace std;
+
+int main() {
+    // Purpose - Create a simple receipt
+    // should also handle sales tax (8%)
+
+    // Declare our variables
+    string item = "Pizza";
+    double item_price = 5.99;
+    double tax_percent = 0.08;  // 8% is 8/100
+    double tax_amount;  // tax in $
+    double total; // price + tax
+
+    // Greet user and take the order
+    cout << "Welcome to our CSC 134 Restaurant!" << endl;
+    cout << "You ordered one " << item << "." << endl;
+
+    // Calculate the meal price
+    // Calculate the sales tax and the total price
+    tax_amount = item_price * tax_percent; // take 8% of the item
+    total = item_price + tax_amount;
+
+    // Print the receipt
+    cout << setprecision(2) << fixed;
+    cout << "Thank you for shopping with us" << endl;
+    cout << "------------------------------" << endl;
+    cout << item << "\t$" << item_price      << endl;
+    cout << "Tax" << "\t\t$" << tax_amount    << endl;
+    cout << "------------------------------" << endl;
+    cout << "Total" << "\t\t$" << total << endl;
+    cout << endl;
+
+    return 0; // no errors
+}
+
+
+    
